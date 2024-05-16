b5d36e3167f86b50ee6b4ff9db90caee29b716a0
Tom Nielsen <tanielsen@gmail.com>
2024-05-16 22:05:53 +0100

    trying to serve static file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use axum::{response::Html, routing::get, routing::post, Json, Router};
+use axum::{
+    response::Html,
+    routing::post,
+    routing::{get, get_service},
+    Json, Router,
+};
 use serde::{Deserialize, Serialize};
+use tower_http::services::ServeDir;
 
 #[derive(Deserialize)]
 struct MyInputs {
@@ -21,9 +27,11 @@ fn do_calc(my_input: MyInputs) -> MyOutputs {
 #[tokio::main]
 async fn main() {
     // build our application with a route
+
     let app = Router::new()
         .route("/", get(index_handler))
-        .route("/calc", post(calc_handler));
+        .route("/calc", post(calc_handler))
+        .nest_service("/assets", get_service(ServeDir::new("assets")));
 
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:4000")
@@ -34,7 +42,7 @@ async fn main() {
 }
 
 async fn index_handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+    Html(include_str!("index.html"))
 }
 
 async fn calc_handler(Json(my_input): Json<MyInputs>) -> Json<MyOutputs> {
