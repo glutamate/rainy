e5c20246ce455ee13e42b332aa7b2fe97d96645e
Tom Nielsen <tanielsen@gmail.com>
2024-05-18 17:44:43 +0100

    nipper

--- src/components.rs
+++ src/components.rs
@@ -1,18 +1,17 @@
 //use html5ever::tree_builder::TreeSink;
-use markup5ever::interface::tree_builder::TreeSink;
-use scraper::{element_ref::ElementRef, node::Element, Html};
+use nipper::{Document, Selection};
 pub fn five() -> f64 {
     5.3
 }
 
 struct Component {
     name: &'static str,
-    process: fn(ElementRef) -> Html,
+    process: fn(Selection) -> Document,
 }
 
 const COMPONENTS: [Component; 1] = [Component {
     name: "xy-plot",
-    process: (|e| Html::parse_fragment("<div>PLOT GOES HERE</div>")),
+    process: (|mut e| Document::from(r#"<div>PLOT GOES HERE</div>"#)),
 }];
 
 /*<div id="myDiv"></div>
@@ -26,28 +25,36 @@ const COMPONENTS: [Component; 1] = [Component {
 </script> */
 
 pub fn process_components(html: &str) -> String {
-    let mut fragment = Html::parse_fragment(html);
+    let mut fragment = Document::from(html);
     //process_components_html(&mut fragment);
 
     for Component { name, process } in COMPONENTS {
-        let selector = scraper::Selector::parse(name).unwrap();
+        let selection = fragment.select(name);
 
         //dbg!(fragment.select(&selector));
 
-        for element in fragment.select(&selector) {
-            let id = element.id();
+        for mut element in selection.iter() {
             let h_substitute = process(element);
-            fragment.append_before_sibling(&id, h_substitute)
+            element.replace_with_selection(&h_substitute.select("*"))
             //dbg!(element.value());
-        }
-        let node_ids: Vec<_> = fragment.select(&selector).map(|x| x.id()).collect();
-        for id in node_ids {
-            fragment.remove_from_parent(&id);
         }
     }
 
-    fragment.html()
+    fragment.html().to_string()
 }
+
+/*fn process_component(Component { name, process }: Component, doc: Document) -> Document {
+    let mut mdoc = Document::from(doc.html());
+    let selection = mdoc.select(name);
+
+    for mut element in selection.iter() {
+        let h_substitute = process(element);
+        //element.replace_with_selection(&h_substitute.select("*"))
+        //dbg!(element.value());
+    }
+    mdoc
+}*/
+
 /*fn process_components_html(fragment: &mut Html) {
     for Component { name, process } in COMPONENTS {
         let selector = scraper::Selector::parse(name).unwrap();
