f8b061778bbccb59aeaa9e936f3aaeae48461e0c
Tom Nielsen <tanielsen@gmail.com>
2024-06-13 09:56:17 +0100

    runtime better

--- saltcorn-module/public/rainy-runtime.js
+++ saltcorn-module/public/rainy-runtime.js
@@ -25,7 +25,7 @@ const rainyEvalExpr = (expr) => {
 
 async function fetchRender() {
   const viewname = document
-    .querySelectorAll("div.rainy-dashboard")
+    .querySelectorAll("div.rainy-dashboard")[0]
     .getAttribute("data-viewname");
   console.log("vn", viewname);
   document
@@ -33,36 +33,29 @@ async function fetchRender() {
     .forEach((el, i) => {
       inputStore[el.name] = el.type === "number" ? +el.value : el.value;
     });
-  const response = await fetch("/calc", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(inputStore),
-  });
-  const data = await response.json();
-  globalRainyContext = { ...inputStore, ...data };
-
-  //render
-  document
-    .querySelectorAll('script[type="text/rainy-loop-js"]')
-    .forEach((el, i) => {
-      const f = new Function(
-        Object.keys(globalRainyContext).join(","),
-        el.textContent
-      );
-      f(...Object.values(globalRainyContext));
+  view_post(viewname, "update", inputStore, (data) => {
+    globalRainyContext = { ...inputStore, ...data };
+    //render
+    document
+      .querySelectorAll('script[type="text/rainy-loop-js"]')
+      .forEach((el, i) => {
+        const f = new Function(
+          Object.keys(globalRainyContext).join(","),
+          el.textContent
+        );
+        f(...Object.values(globalRainyContext));
+      });
+    document.querySelectorAll("[data-show-if]").forEach((el, i) => {
+      const expr = el.getAttribute("data-show-if");
+      const show_it = rainyEvalExpr(expr);
+      if (show_it) el.style.display = "";
+      else el.style.display = "none";
     });
-  document.querySelectorAll("[data-show-if]").forEach((el, i) => {
-    const expr = el.getAttribute("data-show-if");
-    const show_it = rainyEvalExpr(expr);
-    if (show_it) el.style.display = "";
-    else el.style.display = "none";
-  });
 
-  document.querySelectorAll("[data-eval-expr]").forEach((el, i) => {
-    const expr = el.getAttribute("data-eval-expr");
+    document.querySelectorAll("[data-eval-expr]").forEach((el, i) => {
+      const expr = el.getAttribute("data-eval-expr");
 
-    el.textContent = rainyEvalExpr(expr);
+      el.textContent = rainyEvalExpr(expr);
+    });
   });
 }
