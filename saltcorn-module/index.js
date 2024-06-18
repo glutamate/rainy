4d0546c0e15f7804cf4ae3313591832c01a98ffe
Tom Nielsen <tanielsen@gmail.com>
2024-06-18 17:05:48 +0100

    set extra values

--- saltcorn-module/index.js
+++ saltcorn-module/index.js
@@ -7,6 +7,11 @@ const FieldRepeat = require("@saltcorn/data/models/fieldrepeat");
 const Workflow = require("@saltcorn/data/models/workflow");
 const proc_html = require("./templates.js");
 const { pre, code, div, script, domReady } = require("@saltcorn/markup/tags");
+const {
+  jsexprToWhere,
+  eval_expression,
+  freeVariables,
+} = require("@saltcorn/data/models/expression");
 
 const exec = require("child_process").exec;
 const path = require("path");
@@ -113,6 +118,24 @@ const configuration_workflow = () =>
                 },
                 showIf: { persistence_table: tables.map((t) => t.name) },
               },
+              {
+                name: "field_values_formula",
+                label: "Row values formula",
+                class: "validate-expression",
+                sublabel:
+                  'Additional field values set on the persistence table. <code>row</code> referes to the view state. For example <code>{dashboard: "Model1", user: user.id, saved_at: new Date()}</code>',
+                type: "String",
+                fieldview: "textarea",
+              },
+              {
+                name: "row_query",
+                label: "Row query",
+                class: "validate-expression",
+                sublabel:
+                  'Additional query when loading exisitng persistence sets. For example <code>{user: user.id, dashboard: "Model1"}</code>',
+                type: "String",
+                fieldview: "textarea",
+              },
             ],
           });
         },
@@ -130,6 +153,7 @@ const run = async (
     inputs_field,
     outputs_field,
     name_field,
+    row_query,
   },
   state,
   extra
@@ -138,8 +162,11 @@ const run = async (
   const foo = await proc_html(await layout.get_contents());
   let persistData = [];
   if (persistence_table) {
+    const qextra = row_query
+      ? eval_expression(row_query, {}, extra.req.user)
+      : {};
     const table = Table.findOne(persistence_table);
-    const rows = await table.getRows({});
+    const rows = await table.getRows(qextra);
     persistData = rows.map((r) => ({
       inputs: r[inputs_field],
       name: r[name_field],
@@ -201,14 +228,25 @@ const update = async (
 const save_persist = async (
   table_id,
   viewname,
-  { persistence_table, inputs_field, outputs_field, name_field },
+  {
+    persistence_table,
+    inputs_field,
+    outputs_field,
+    name_field,
+    field_values_formula,
+  },
   body,
   { req }
 ) => {
   if (persistence_table) {
+    let extraVals = {};
+    if (field_values_formula) {
+      extraVals = eval_expression(field_values_formula, {}, req.user);
+    }
     const table = Table.findOne(persistence_table);
     const row = {};
     await table.insertRow({
+      ...extraVals,
       [inputs_field]: body.inputs,
       [outputs_field]: body.outputs,
       [name_field]: body.name,
